eaf4745a2dfe70cce15c9a0f1aa570a96be7a5b6
CodeRad <coderaddesign@gmail.com>
2024-07-06 11:37:15 -0700

    Simplify auth and protected routing

--- src/pages/ApplicationRoutes.jsx
+++ src/pages/ApplicationRoutes.jsx
@@ -3,12 +3,10 @@ import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 
 // Import public pages to pass to router
 import PublicLayout from "./PublicLayout.jsx";
-
 import Login from "./public/Login.jsx";
 
 // Import pages that require authorization
 import ProtectedLayout from "./ProtectedLayout.jsx";
-
 import Welcome from './protected/Welcome.jsx';
 import CourseSelect from './protected/CourseSelect.jsx';
 import Courseware from './protected/Courseware.jsx';
@@ -23,10 +21,6 @@ import rootStore from "../stores/rootStore.js";
 export default function ApplicationRoutes() {
     const authStore = rootStore.authenticationStore;
     const [isSignedIn, setIsSignedIn] = useState(authStore.signedIn());
-
-    useEffect(() => {
-        setIsSignedIn(authStore.signedIn());
-    }, [authStore]);
 
     return (
         <Routes>
@@ -49,47 +43,23 @@ export default function ApplicationRoutes() {
             />
 
             {/* Protected routes */}
-            <Route element={<ProtectedLayout />}>
+            {isSignedIn && (
+                <Route element={<ProtectedLayout />}>
+                    <Route path="/welcome" element={<Welcome />} />
+                    <Route path="/course-select" element={<CourseSelect />} />
+                    <Route path="/courseware" element={<Courseware />} />
+                    <Route path="/assessment" element={<Assessment />} />
+                    <Route path="/results" element={<Results />} />
+                    <Route path="/feedback" element={<Feedback />} />
+                    <Route path="/finish" element={<Finish />} />
+                </Route>
+            )}
+            {!isSignedIn && (
                 <Route
-                    path="/welcome"
-                    element={<RequireAuth><Welcome /></RequireAuth>}
+                    path="*"
+                    element={<Navigate to="/login" />}
                 />
-                <Route
-                    path="/course-select"
-                    element={<RequireAuth><CourseSelect /></RequireAuth>}
-                />
-                <Route
-                    path="/courseware"
-                    element={<RequireAuth><Courseware /></RequireAuth>}
-                />
-                <Route
-                    path="/assessment"
-                    element={<RequireAuth><Assessment /></RequireAuth>}
-                />
-                <Route
-                    path="/results"
-                    element={<RequireAuth><Results /></RequireAuth>}
-                />
-                <Route
-                    path="/feedback"
-                    element={<RequireAuth><Feedback /></RequireAuth>}
-                />
-                <Route
-                    path="/finish"
-                    element={<RequireAuth><Finish /></RequireAuth>}
-                />
-            </Route>
+            )}
         </Routes>
     );
-}
-
-function RequireAuth({ children }) {
-    const authStore = rootStore.authenticationStore;
-    const location = useLocation();
-
-    if (!authStore.signedIn()) {
-        return <Navigate to="/login" state={{ from: location }} />;
-    }
-
-    return children;
 }
