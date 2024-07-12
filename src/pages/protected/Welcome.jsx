7451099e9f4139f112b3d44619d1fabc1430f6d0
CodeRad <coderaddesign@gmail.com>
2024-07-12 09:58:46 -0700

    Display clientname from group GET

--- src/pages/protected/Welcome.jsx
+++ src/pages/protected/Welcome.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 import rootStore from "../../stores/rootStore.js";
+import { courseApi } from "../../api/course.js";
 
 export default function Welcome() {
     const navigate = useNavigate();
     const clientName = "FBC Industreinforce";
     const [agreed, setAgreed] = useState(false);
+    const [employeeGroup, setEmployeeGroup] = useState(null);
+
+    const getEmployeeGroup = async () => {
+        try {
+            const group = await courseApi.getEmployeeGroup();
+            setEmployeeGroup(group);
+            console.log(group);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     const handleNext = () => {
         navigate('/course-select');
@@ -17,11 +29,16 @@ export default function Welcome() {
         setAgreed(e.target.checked);
     };
 
+    useEffect(() => {
+        getEmployeeGroup();
+    }, []);
+
+
     return (
         <Col>
             <h1>Welcome</h1>
             <hr />
-            <p>Welcome to the LMS Training Portal for {clientName}.</p>
+            <p>Welcome to the LMS Training Portal for {employeeGroup && employeeGroup.clientName}.</p>
             <p>To continue, please check the box below and proceed to course select.</p>
             
             <Form className="">
