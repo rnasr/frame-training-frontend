cc528111dd09da5428e0b6951c65647875e646f1
CodeRad <coderaddesign@gmail.com>
2024-07-17 17:34:17 -0700

    Add text and styling for courseware screen

--- src/pages/protected/Courseware.jsx
+++ src/pages/protected/Courseware.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Button } from "react-bootstrap";
+import { Row, Col, Button, Alert } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 export default function Courseware() {
@@ -12,8 +12,13 @@ export default function Courseware() {
 	return (
 		<Col>
 			<h1>Courseware</h1>
-			<p>Placeholder content for course materials and learning modules.</p>
-			<Button onClick={handleNext}>Go to Assessment</Button>
+			<hr />
+			<Alert variant="info" className="mt-5">
+			<Row className="ms-1 my-4">The course you have chosen should have popped up in a new window. If the course did not open, turn off your pop-up blocker for this site and click here to relaunch.</Row>
+			<Row className="ms-1 my-4"><strong>Please keep this window open while you are taking the course.</strong></Row>
+			<Row className="ms-1 my-4">Once you have finished, click the button below to go to the Assessment section.</Row>
+			</Alert>
+			<Button className='mt-5 w-100' onClick={handleNext}>Go to Assessment</Button>
 		</Col>
 	);
 }
