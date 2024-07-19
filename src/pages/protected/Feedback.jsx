5cd785a29d3483ef35000dd87ec12f5b36377361
CodeRad <coderaddesign@gmail.com>
2024-07-18 23:14:38 -0700

    Add Feedback questions

--- src/pages/protected/Feedback.jsx
+++ src/pages/protected/Feedback.jsx
@@ -1,19 +1,77 @@
-import React from "react";
-import { Col, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Col, Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import * as formik from 'formik';
+
+import { courseApi } from "../../api/course.js";
 
 export default function Feedback() {
-	const navigate = useNavigate();
+    const { Formik } = formik;   
+    const navigate = useNavigate();
+    
+    const [feedbackQuestions, setFeedbackQuestions] = useState([]);
+    
+    const getFeedbackQuestions = async () => {
+        try {
+            const questions = await courseApi.getFeedbackQuestions();
+            setFeedbackQuestions(questions);
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
-	const handleNext = () => {
-		navigate("/finish");
-	};
+    useEffect(() => {
+        getFeedbackQuestions();
+    }, []);
 
-	return (
-		<Col>
-			<h1>Feedback</h1>
-			<p>Placeholder content for optional user feedback section.</p>
-			<Button onClick={handleNext}>Finish Course</Button>
-		</Col>
-	);
+    const submitFeedback = async (values) => {
+        try {
+            await courseApi.submitFeedback(values);
+            handleNext();
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const handleNext = () => {
+        navigate("/certificate");
+    };
+
+    return (
+        <Col>
+            <h1>Feedback</h1>
+            <hr />
+            <Formik
+                initialValues={{}}
+                onSubmit={(values, { setSubmitting }) => {
+                    submitFeedback(values);
+                    setSubmitting(false);
+                }}
+            >
+                {({ handleSubmit, handleChange, values }) => (
+                    <Form noValidate onSubmit={handleSubmit}>
+                        {feedbackQuestions.map((question) => (
+                            <Form.Group controlId={question.id} key={question.id}>
+                                <Form.Label className="mt-3">{question.question}</Form.Label>
+                                {question.options.map(option => (
+                                    <Form.Check
+                                        type="radio"
+                                        label={option.text}
+                                        name={question.id}
+                                        value={option.id}
+                                        checked={values[question.id] === String(option.id)}
+                                        onChange={handleChange}
+                                        key={option.id}
+                                    />
+                                ))}
+                            </Form.Group>
+                        ))}
+                        <Button className="w-100 mt-5" type="submit">
+                            Submit Feedback and Get Certificate
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Col>
+    );
 }
